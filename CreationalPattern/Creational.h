85f93ebf50507adce11c5bb172503f54911b428e
Admin <Admin@Admin-Laptop>
2017-11-17 22:26:30 -0400

    Initial version.

---
+++ CreationalPattern/Creational.h
@@ -0,0 +1,14 @@
+/*
+ * Creational.h
+ *
+ *  Created on: Nov 17, 2017
+ *      Author: Admin
+ */
+
+#ifndef CREATIONAL_H_
+#define CREATIONAL_H_
+
+class Creational {
+};
+
+#endif /* CREATIONAL_H_ */
